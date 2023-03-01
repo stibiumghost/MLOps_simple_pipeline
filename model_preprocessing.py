ad7336265b6f3647f7f0648a67c24e0791ca42e1
SectorSpark <sectorspark@gmail.com>
2023-03-01 11:50:44 +0500

    Remove fit to test data

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -4,10 +4,12 @@ from get_files import file_names
 
 train_files = file_names('train')
 test_files = file_names('test')
-files = train_files + test_files
 
 # Apply scaler to values
 scaler = StandardScaler()
-for file in files:
+for file in train_files:
     X = np.genfromtxt(file[0], delimiter=',')
     np.savetxt(file[0], scaler.fit_transform(X), delimiter=',', fmt='%.3e')
+for file in test_files:
+    X = np.genfromtxt(file[0], delimiter=',')
+    np.savetxt(file[0], scaler.transform(X), delimiter=',', fmt='%.3e')
