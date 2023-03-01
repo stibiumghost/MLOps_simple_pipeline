2e996dc113947bf1a3cb07cf67a1c8cec3be1a45
SectorSpark <sectorspark@gmail.com>
2023-03-01 11:41:38 +0500

    Add second step

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -1,1 +1,13 @@
-# sklearn.preprocessing.StandardScaler <-- as an example
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from get_files import file_names
+
+train_files = file_names('train')
+test_files = file_names('test')
+files = train_files + test_files
+
+# Apply scaler to values
+scaler = StandardScaler()
+for file in files:
+    X = np.genfromtxt(file[0], delimiter=',')
+    np.savetxt(file[0], scaler.fit_transform(X), delimiter=',', fmt='%.3e')
