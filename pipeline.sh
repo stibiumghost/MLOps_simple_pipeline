be31dd7288f3194b5b4708bd92db5cad5bf33352
stibiumghost <116355639+stibiumghost@users.noreply.github.com>
2023-03-02 12:22:55 +0500

    python -> python3

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python data_creation.py
-python model_preprocessing.py
-python model_preparation.py
-python model_testing.py
+python3 data_creation.py
+python3 model_preprocessing.py
+python3 model_preparation.py
+python3 model_testing.py
