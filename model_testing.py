f3b6414ea9f1fc0afd1dc881f82df0f48ba05c7c
rulthw <rulthw.247@gmail.com>
2023-03-05 17:09:55 +0500

    Add saving of predictions

--- model_testing.py
+++ model_testing.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from pathlib import Path
 from sklearn.metrics import accuracy_score
 from get_files import file_names
 
@@ -13,7 +14,9 @@ with open('model.txt', 'rb') as file:
     model = pickle.load(file)
 
 
-# predict & print out results
+# Save predictions & print out results
+Path('predict').mkdir(parents=True, exist_ok=True)
+
 for file in files:
     X_test = np.genfromtxt(file[0], delimiter=',')
     X_test_all.extend(X_test)
@@ -21,10 +24,16 @@ for file in files:
     y_test = np.genfromtxt(file[1], delimiter=',')
     y_test_all.extend(y_test)
 
+    prediction = model.predict(X_test)
+    np.savetxt('predict/' + file[0][5:] + '_predict', prediction, delimiter=',', fmt='%.3e')
+
     print(f'For {file[0][5:]} ({len(y_test)} lines) accuracy score is', \
-        accuracy_score(y_test, model.predict(X_test)))
+        accuracy_score(y_test, prediction))
+
+prediction_all = model.predict(X_test_all)
+np.savetxt('predict/X_test_all_predict', prediction_all, delimiter=',', fmt='%.3e')
 
 print(f'Combined ({len(y_test_all)} lines) accuracy score is', \
-    accuracy_score(y_test_all, model.predict(X_test_all)))
+    accuracy_score(y_test_all, prediction_all))
 
 input() # wait for input to be able to check metrics
