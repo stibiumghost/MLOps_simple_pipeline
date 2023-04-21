1ad58f6ae1bbfebeaa45a77f7b2f2db7dae8cb68
SectorSpark <sectorspark@gmail.com>
2023-04-21 14:21:25 +0500

    dvc setup, titanic data creation

---
+++ lab4/titanic_data_creation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from catboost.datasets import titanic
+
+# Load the Titanic dataset from CatBoost
+train_df, test_df = titanic()
+
+# Save the training and testing data frames as CSV files
+train_df.to_csv("train_df.csv", index=False)
+test_df.to_csv("test_df.csv", index=False)
+
+print('done')
