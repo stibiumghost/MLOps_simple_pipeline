b1b3944563dfa64474a8db369f82f8f41e146966
SB <whjet@mail.ru>
2023-04-24 17:41:09 +0500

    NaN to mean (for Age)

--- lab4/titanic.py
+++ lab4/titanic.py
@@ -6,4 +6,7 @@ titanic_df = pd.concat([pd.read_csv("train_df.csv")[["Pclass", "Sex", "Age"]],
                         pd.read_csv("test_df.csv")[["Pclass", "Sex", "Age"]]],
                         sort=True).reset_index(drop=True)
 
+# NaN to mean (for Age)
+titanic_df.Age = titanic_df.Age.fillna(titanic_df.Age.mean())
+
 titanic_df.to_csv("titanic_df.csv", index=False)
