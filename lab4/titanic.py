0e13333a86a4ed63f4a6f7454b702d7c99fac83f
SB <whjet@mail.ru>
2023-04-24 18:09:54 +0500

    Version 3 of titanic_df

--- lab4/titanic.py
+++ lab4/titanic.py
@@ -9,4 +9,9 @@ titanic_df = pd.concat([pd.read_csv("train_df.csv")[["Pclass", "Sex", "Age"]],
 # NaN to mean (for Age)
 titanic_df.Age = titanic_df.Age.fillna(titanic_df.Age.mean())
 
+# Categorical to on hot encoding (for Sex)
+sexes = pd.get_dummies(titanic_df.Sex)
+titanic_df[sexes.columns.to_list()] = sexes[sexes.columns]
+titanic_df.drop(columns=["Sex"], inplace=True)
+
 titanic_df.to_csv("titanic_df.csv", index=False)
